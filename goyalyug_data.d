78c5404f9d43500742ca18637aa1d4b912e95feb
yugamgoyal <yug.goyal46@gmail.com>
2023-04-19 03:41:51 -0500

    pls babe

--- goyalyug_data.d
+++ goyalyug_data.d
@@ -1,1 +1,1 @@
-goyalyug.data: goyalyug.dir goyalyug.dir/dark side of the moon goyalyug.dir/swift goyalyug.dir/new romantics goyalyug.dir/travis.wav goyalyug.dir/travis goyalyug.dir/swift.wav
+goyalyug.data: goyalyug.dir goyalyug.dir/dark side of the moon goyalyug.dir/swift goyalyug.dir/new romantics goyalyug.dir/swift_ goyalyug.dir/travis goyalyug.dir/travis_
--- kernel/build/goyalyug.d
+++ kernel/build/goyalyug.d
@@ -1,9 +1,8 @@
-build/goyalyug.o: \
- /u/krathod/439/final_project/Audio-Output-Final/goyalyug.cc ide.h \
+build/goyalyug.o: /u/goyalyug/OS429/Audio-Output-Final/goyalyug.cc ide.h \
  stdint.h block_io.h debug.h \
  /u/gheith/public/gcc-11.2.0/lib/gcc/x86_64-linux-gnu/11.2.0/include/stdarg.h \
  io.h atomic.h machine.h init.h loop.h ext2.h ide.h shared.h smp.h \
  config.h libk.h blocking_lock.h semaphore.h queue.h threads.h heap.h \
  libk.h threads.h semaphore.h future.h pit.h \
- /u/krathod/439/final_project/Audio-Output-Final/list_wave.h physmem.h \
- vga.h u8250.h kernel.h pit.h idt.h crt.h port.h ext2.h kb.h vga.h
+ /u/goyalyug/OS429/Audio-Output-Final/list_wave.h physmem.h vga.h u8250.h \
+ kernel.h pit.h idt.h crt.h port.h ext2.h kb.h vga.h
