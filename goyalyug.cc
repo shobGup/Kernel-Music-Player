620b2a123d66f0f0d54f749f690aa88244f7af65
yugamgoyal <yug.goyal46@gmail.com>
2023-04-18 23:59:02 -0500

    hello

--- goyalyug.cc
+++ goyalyug.cc
@@ -534,7 +534,7 @@ void kernelMain(void) {
             written = 0; 
             index = 0; 
             Debug::printf("Before Offset: %d\n", wave_file.offset);
-            wave_file.offset -= (wave_file.offset - wave_file.reset_offset) > (4096 * 16) ? (4096 * 16) : (wave_file.offset - wave_file.reset_offset);
+            wave_file.offset -= (wave_file.offset - wave_file.reset_offset) > (4096 * 16 * 100) ? (4096 * 16 * 100) : (wave_file.offset - wave_file.reset_offset);
             Debug::printf("After Offset: %d\n", wave_file.offset);
             reset(wave_file);
             Debug::printf("Should be change buffer\n");
