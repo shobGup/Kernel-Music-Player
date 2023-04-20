b4ebc732e7dedb029c99ff7bf5437b4f1de8b003
Kavya Rathod <krathod@cs.utexas.edu>
2023-04-20 12:08:23 -0500

    jdkhfgd

---
+++ kernel/build/smp.d
@@ -0,0 +1,4 @@
+build/smp.o: smp.cc smp.h config.h stdint.h atomic.h machine.h init.h \
+ loop.h debug.h \
+ /u/gheith/public/gcc-11.2.0/lib/gcc/x86_64-linux-gnu/11.2.0/include/stdarg.h \
+ io.h idt.h
---
+++ kernel/build/threads.d
@@ -0,0 +1,5 @@
+build/threads.o: threads.cc debug.h \
+ /u/gheith/public/gcc-11.2.0/lib/gcc/x86_64-linux-gnu/11.2.0/include/stdarg.h \
+ io.h stdint.h atomic.h machine.h init.h loop.h smp.h config.h ext2.h \
+ ide.h block_io.h shared.h libk.h blocking_lock.h semaphore.h queue.h \
+ threads.h heap.h
