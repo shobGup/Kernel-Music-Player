5f7c8a88bad2e2f38f3c1136f37023b0d2604b5c
Kavya Rathod <krathod@cs.utexas.edu>
2023-04-20 23:17:44 -0500

    idk what atta added

---
+++ kernel/build/ext2.d
@@ -0,0 +1,4 @@
+build/ext2.o: ext2.cc ext2.h ide.h stdint.h block_io.h debug.h \
+ /u/gheith/public/gcc-11.2.0/lib/gcc/x86_64-linux-gnu/11.2.0/include/stdarg.h \
+ io.h atomic.h machine.h init.h loop.h shared.h smp.h config.h libk.h \
+ blocking_lock.h semaphore.h queue.h threads.h heap.h
